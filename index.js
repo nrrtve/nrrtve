0c0111f6278b67ba2b14308e2e23e9b48e150d91
nrrtve <nrrrtve@gmail.com>
2024-12-21 10:45:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
